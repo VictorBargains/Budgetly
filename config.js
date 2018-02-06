6668d65aca35e1381682916207539df1a9b9ac9a
Victor B Andersen <v@victorb.net>
2018-02-05 21:39:32 -0800

    stop being stupid about secret keys

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 exports.DATABASE_URL =
   process.env.DATABASE_URL ||
   global.DATABASE_URL ||
-  "mongodb://node-First:12perfecto@ds149134.mlab.com:49134/budgetly";
+  "mongodb://localhost/jwt-auth-budgetly-app";
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost/jwt-auth-budgetly-app-test"
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET || "itistobekeptsecret";
+exports.JWT_SECRET = process.env.JWT_SECRET || "";
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || "7d";
